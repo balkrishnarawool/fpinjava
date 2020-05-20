7ce2c80e5ea1fbea9b2485a872fff27000987395
Balkrishna <balkrishna.rawool@gmail.com>
2020-05-20 09:33:42 +0200

    Some more functions added to TailCallExamples

--- src/main/java/fpinjava/chapter4/TailCallExamples.java
+++ src/main/java/fpinjava/chapter4/TailCallExamples.java
@@ -1,10 +1,13 @@
 package fpinjava.chapter4;
 
 import fpinjava.chapter2.Function;
+import fpinjava.chapter3.CollectionUtilities;
 
 import java.math.BigInteger;
 import java.util.List;
 
+import static fpinjava.chapter2.Function.compose;
+import static fpinjava.chapter2.Function.identity;
 import static fpinjava.chapter3.CollectionUtilities.*;
 import static fpinjava.chapter4.TailCall.ret;
 import static fpinjava.chapter4.TailCall.sus;
@@ -58,7 +61,7 @@ public abstract class TailCallExamples<T> {
     }
 
     public static TailCall<List<Integer>> range_(Integer start, Integer end, List<Integer> list) {
-        return (start > end)
+        return (start.equals(end))
                 ? ret(list)
                 : sus( () -> range_(start + 1, end, append(list, start)));
     }
@@ -72,4 +75,48 @@ public abstract class TailCallExamples<T> {
                 ? ret(accumulator)
                 : sus( () ->  foldRight_(tail(ts), f.apply(head(ts)).apply(accumulator), f));
     }
+
+    // Exercise 4.6
+    // This is not tail-recursive: My solution 1
+    public static <T> Function<T, T> composeAllNaive(List<Function<T, T>> list) {
+        return list.isEmpty()
+                ? identity()
+                : compose(head(list), composeAllNaive(tail(list)));
+    }
+
+    // Stack safe : My solution 2
+    public static <T> Function<T, T> composeAllStackSafe(List<Function<T, T>> list) {
+        return composeAllStackSafe_(reverseFoldLeft(list), identity()).eval();
+    }
+
+    public static <T> TailCall<Function<T, T>> composeAllStackSafe_(List<Function<T, T>> list, Function<T, T> accumulator) {
+        return list.isEmpty()
+                ? ret(accumulator)
+                : sus(() -> composeAllStackSafe_(tail(list), compose(head(list), accumulator)));
+    }
+
+    // From above two solutions it is clear that composeAll requires right folding
+    public static <T> Function<T, T> composeAll(List<Function<T, T>> list) {
+//        return CollectionUtilities.foldRightRecurs1(list, identity(), f1 -> f2 -> f1.compose(f2));// For lists with around 8500 elements, this will throw StackOverflowError
+        return foldRight(list, identity(), f1 -> f2 -> f1.compose(f2));// So use this.
+    }
+
+    public static <T> Function<T, T> composeAllViaFoldLeft(List<Function<T, T>> list) {
+        return t -> foldLeft(reverseFoldLeft(list), t, t1 -> f -> f.apply(t1));
+        // Question: Why is 't' used here for identity?
+        // That is because when the list is empty we just want to give the same input back.
+    }
+
+    public static <T> Function<T, T> composeAllViaFoldRight(List<Function<T, T>> list) {
+        return t -> foldRight(list, t, f -> f::apply); // f::apply = t1 -> f.apply(t1)
+    }
+
+    public static <T> Function<T, T> andThenAllViaFoldLeft(List<Function<T, T>> list) {
+        return t -> foldLeft(list, t, t1 -> f -> f.apply(t1));
+    }
+
+    public static <T> Function<T, T> andThenAllViaFoldRight(List<Function<T, T>> list) {
+        return t -> foldRight(reverseFoldLeft(list), t, f -> f::apply);
+    }
+
 }
--- src/test/java/fpinjava/chapter4/TailCallExamplesTest.java
+++ src/test/java/fpinjava/chapter4/TailCallExamplesTest.java
@@ -1,11 +1,14 @@
 package fpinjava.chapter4;
 
+import fpinjava.chapter2.Function;
 import fpinjava.chapter3.CollectionUtilities;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.List;
 
+import static fpinjava.chapter3.CollectionUtilities.list;
+import static fpinjava.chapter3.CollectionUtilities.map;
 import static fpinjava.chapter4.TailCallExamples.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -39,8 +42,32 @@ public class TailCallExamplesTest {
     }
 
     @Test
-    public void testRangeImper() {
-        assertEquals(numbers, range(1, 5));
+    public void testRange() {
+        assertEquals(numbers, range(1, 6));
+    }
+
+    @Test
+    public void testComposeAll() {
+        final Function<Integer, Integer> add2 = n -> n + 2;
+        final Function<Integer, Integer> square = n -> n * n;
+        final Function<Integer, Integer> cube = n -> n * n * n;
+
+        final List<Function<Integer, Integer>> list = list(add2, square, cube);
+
+        assertEquals(66, composeAllNaive(list).apply(2));
+        assertEquals(66, composeAllStackSafe(list).apply(2));
+        assertEquals(66, composeAll(list).apply(2));
+
+        Function<Integer, Integer> add = n -> n + 1;
+        assertEquals(500, composeAll(map(range(0, 500), x -> add)).apply(0));
+        // This will give StackOverflowError for recursive implementation of foldRight.
+        // And will take long time when foldRight is stack-safe. So it is commented.
+        // assertEquals(10000, composeAll(map(range(0, 10000), x -> add)).apply(0));
+
+        assertEquals(500, composeAllViaFoldLeft(map(range(0, 500), x -> add)).apply(0));
+        assertEquals(500, composeAllViaFoldRight(map(range(0, 500), x -> add)).apply(0));
+        assertEquals(500, andThenAllViaFoldLeft(map(range(0, 500), x -> add)).apply(0));
+        assertEquals(500, andThenAllViaFoldRight(map(range(0, 500), x -> add)).apply(0));
     }
 
 }
