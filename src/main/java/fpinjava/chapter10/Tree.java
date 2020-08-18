ef613eaa7bb60809dcdc4ff71e2ffa5bc09b481a
Balkrishna <balkrishna.rawool@gmail.com>
2020-08-18 12:42:04 +0200

    Starting with Tree

---
+++ src/main/java/fpinjava/chapter10/Tree.java
@@ -0,0 +1,27 @@
+package fpinjava.chapter10;
+
+// TODO
+// List has limitations:
+// - The complexity of accessing elements grows proportionally with the number of elements.
+// For example, searching for a particular element may necessitate examining all elements
+// if it happens that the searched-for element is the last in the list.
+// - Among other less efficient operations are sorting, accessing elements by their index,
+// and finding the maximal or minimal element.
+// Definitions:
+// - Tree
+// - Element/ Node
+// - Binary tree
+// - Branch: Left branch, Right branch
+// - Subtree
+// - Root
+// - Leaf
+// - Full tree
+// - Perfectly balanced tree/ Perfect tree
+// - Imperfectly balanced tree
+// - Perfectly imbalanced tree
+// - Size
+// - Height
+// - Depth
+// Height and depth of an empty tree are equal to -1.
+public abstract class Tree<A> {
+}
