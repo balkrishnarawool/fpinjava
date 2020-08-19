609f9fa780d1d71f9038ca4b1d0882f369a10a86
Balkrishna <balkrishna.rawool@gmail.com>
2020-08-19 14:16:40 +0200

    some notes added for Tree

--- src/main/java/fpinjava/chapter10/Tree.java
+++ src/main/java/fpinjava/chapter10/Tree.java
@@ -23,5 +23,12 @@ package fpinjava.chapter10;
 // - Height
 // - Depth
 // Height and depth of an empty tree are equal to -1.
+// Leafy tree: A tree that only contains data at the leaf nodes
+// Ordered Binary Tree / Binary Search Tree
+// A recursive search in a perfectly balanced binary tree will never overflow the stack.
+// - Because the standard stack size allows for minimum 1000 recursive calls/ stack frames.
+// - A perfectly balanced binary tree of height 1000 contains 2^1000 elements, you’ll never have enough main memory for such data.
+// - But the problem is not all trees are perfectly balanced and therefore we need a mechanism to balance them.
+// - A perfectly imbalanced tree is in fact a singly linked list
 public abstract class Tree<A> {
 }
